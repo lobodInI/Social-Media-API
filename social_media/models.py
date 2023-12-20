f3a1d0eb36cf3f5edb468727ae9cbd578e331e5c
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-21 01:39:08 +0200

    created models for social_media app

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,100 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.text import slugify
+
+
+def user_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.nickname)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads", "users", filename)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="user_profile"
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
+    bio = models.TextField(null=True, blank=True)
+    user_image = models.ImageField(
+        null=True, blank=True, upload_to=user_image_file_path
+    )
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return f"{self.full_name}({self.nickname})"
+
+
+class Follow(models.Model):
+    following = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="following"
+    )
+    follower = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="follower"
+    )
+    followed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("following", "follower")
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    scheduled_at = models.DateTimeField(
+        null=True, blank=True, default=None
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return f"Post by {self.author} at {self.created_at}"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
