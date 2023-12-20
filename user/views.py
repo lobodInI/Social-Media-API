d84331b8095f23d8c1f6250611d5c0f82229230e
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-20 20:07:21 +0200

    created views for user model

--- user/views.py
+++ user/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+class LogoutUserView(APIView):
+    """Access token expiry as short as possible"""
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        token = RefreshToken(request.data.get("refresh"))
+        token.blacklist()
+        return Response("Logged out successfully", status=status.HTTP_200_OK)
