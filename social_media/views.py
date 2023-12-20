ab4622e69d74dad9e56ba3b1ce630b32b92d08c2
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-21 01:43:47 +0200

    created viewsets for model UserFrofile

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from social_media.models import UserProfile
+from social_media.serializers import (
+    UserProfileSerializer,
+    UserProfileListSerializer,
+    UserProfileDetailSerializer,
+    UserProfileImageSerializer
+)
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return UserProfileListSerializer
+        if self.action == "retrieve":
+            return UserProfileDetailSerializer
+        if self.action == "upload_image":
+            return UserProfileImageSerializer
+
+        return UserProfileSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAuthenticated],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific movie"""
+        profile = self.get_object()
+        serializer = self.get_serializer(profile, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
